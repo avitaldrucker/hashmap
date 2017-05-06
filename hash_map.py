a7b188221d04e91f62093650d5f479573eae1cd1
Avital Drucker <avitaldrucker@gmail.com>
2017-05-06 17:38:14 -0400

    Add LinkedList and HashMap classes

---
+++ hash_map.py
@@ -0,0 +1,94 @@
+from linked_list import LinkedList
+
+class HashMap(object):
+
+    def __init__(self, num_buckets=None):
+        if num_buckets is None:
+            num_buckets = 8
+
+
+        self.store = [LinkedList()] * num_buckets
+        self.count = 0
+
+
+    def include(self, key):
+        linked_list = self.store[hash(key) % len(self.store)]
+
+        return linked_list.include(key)
+
+    def set(self, key, val):
+        linked_list = self.store[hash(key) % len(self.store)]
+
+        if self.include(key):
+            linked_list.update(key, val)
+        else:
+            if self.count == len(self.store):
+                self.resize()
+            linked_list.append(key, val)
+            self.count = self.count + 1
+
+    def get(self, key):
+        return self.store[hash(key) % len(self.store)].get(key)
+
+    def delete(self, key):
+        linked_list = self.store[hash(key) % len(self.store)]
+
+        if linked_list.include(key):
+            linked_list.remove(key)
+            self.count = self.count - 1
+
+    def resize(self):
+        old_store = self.store
+        self.count = 0
+        self.store = [LinkedList()] * (len(self.store))
+
+        for linked_list in old_store:
+            for link in linked_list:
+                self.set(link.key, link.val)
+
+def hashMaker ():
+    some_hash = HashMap()
+    some_hash.set("first", 1)
+    some_hash.set("second", 2)
+    some_hash.set("third", 3)
+    return some_hash
+
+print("")
+print("HASH MAP")
+print("[]")
+print("gets by key")
+some_hash = hashMaker()
+print(some_hash.get("first") == 1)
+print(some_hash.get("second") == 2)
+print(some_hash.get("third") == 3)
+
+
+print("")
+print("[]=")
+some_hash_1 = hashMaker()
+print('sets a key value pair')
+some_hash_1.set("fourth", 4)
+print(some_hash_1.get(4) == None)
+print(some_hash_1.get("fourth") == 4)
+
+print("overwrites any existing value for the given key")
+some_hash_2 = hashMaker()
+some_hash_2.set("one", 1)
+some_hash_2.set("one", "one")
+print(some_hash_2.get("one") == "one")
+
+print("works with various data types")
+some_hash_3 = hashMaker()
+some_hash_3.set("fourth", 4)
+some_hash_3.set(5, 5)
+print(some_hash_3.get("fourth") == 4)
+print(some_hash_3.get(5) == 5)
+
+print("")
+print("include")
+print("returns true if a key is present")
+some_hash_4 = hashMaker()
+print(some_hash_4.include("first") == True)
+
+print("returns false if a key is not in the hash")
+print(some_hash_4.include("fourth") == False)
--- linked_list.py
+++ linked_list.py
@@ -6,25 +6,160 @@ class Link(object):
         self.next = None
         self.prev = None
 
-link = Link()
-print(link.key)
-print(link.val)
-print(link.next)
-print(link.prev)
-print(link.to_s)
 
 
-# class MyStuff(object):
-#
-#     def __init__(self):
-#         self.tangerine = "And now a thousand years between"
-#
-#     def apple(self):
-#         print "I AM CLASSY APPLES!"
-#
-#
-#         def append_to(element, to=None):
-#     if to is None:
-#         to = []
-#     to.append(element)
-#     return to
+class LinkedList(object):
+
+    def __init__(self):
+        self.head = Link()
+        self.tail = Link()
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+
+    def first(self):
+        return self.head.next
+
+    def last(self):
+        return self.tail.prev
+
+    def empty(self):
+        return self.head.next == self.tail
+
+    def get(self, key):
+        link = self.head
+
+        while link != self.tail:
+            if link.key == key:
+                break
+            link = link.next
+
+        return link.val
+
+
+    def include(self, key):
+        link = self.head
+
+        while link != self.tail:
+            if link.key == key:
+                return True
+            link = link.next
+
+
+        return False
+
+    def append(self, key, val):
+        old_prev = self.tail.prev
+
+        new_link = Link(key, val)
+        self.tail.prev = new_link
+        new_link.next = self.tail
+        new_link.prev = old_prev
+        old_prev.next = new_link
+
+    def update(self, key, val):
+        link = self.head
+
+        while link != self.tail:
+            if link.key == key:
+                break
+            link = link.next
+
+        if link != self.tail:
+            link.val = val
+
+        return link
+
+    def remove(self, key):
+        link = self.head
+
+        while link != self.tail:
+            if link.key == key:
+                break
+
+            link = link.next
+
+        if link != self.tail:
+            link.next.prev = link.prev
+            link.prev.next = link.next
+
+        return link
+
+
+
+
+
+
+empty_list = LinkedList()
+print(empty_list.empty() == True)
+
+linked_list = LinkedList()
+linked_list.append("first", 1)
+linked_list.append("second", 2)
+linked_list.append("third", 3)
+
+print(linked_list.empty() == False)
+
+empty_list.append("first", 1)
+print(empty_list.empty() == False)
+
+print(linked_list.first().key == "first")
+print(linked_list.last().key == "third")
+
+# Another test
+empty_list_2 = LinkedList()
+empty_list_2.append("first", 1)
+empty_list_2.update("first", 2)
+print("updates links")
+print(empty_list_2.first().val == 2)
+
+# Another test
+print("doesn't add new links")
+empty_list_3 = LinkedList()
+empty_list_3.update("first", 2)
+print(empty_list_3.empty() == True)
+
+print("get")
+print("gets by key")
+linked_list_2 = LinkedList()
+linked_list_2.append("first", 1)
+linked_list_2.append("second", 2)
+linked_list_2.append("third", 3)
+print(linked_list_2.get("first") == 1)
+print(linked_list_2.get("second") == 2)
+print(linked_list_2.get("third") == 3)
+
+print("returns nil for absent keys")
+print(linked_list_2.get("absent key") == None)
+
+print("remove")
+print("removes a link by key")
+linked_list_3 = LinkedList()
+linked_list_3.append("first", 1)
+linked_list_3.append("second", 2)
+linked_list_3.append("third", 3)
+
+print(linked_list_3.get("first") == 1)
+linked_list_3.remove("first")
+print(linked_list_3.get("first") == None)
+
+
+print("reassigns pointers when links are removed")
+linked_list_4 = LinkedList()
+linked_list_4.append("first", 1)
+linked_list_4.append("second", 2)
+linked_list_4.append("third", 3)
+linked_list_4.remove("second")
+print(linked_list_4.first().next.key == "third")
+print(linked_list_4.last().prev.key == "first")
+
+print("include")
+print("returns true if a key is present")
+linked_list_5 = LinkedList()
+linked_list_5.append("first", 1)
+linked_list_5.append("second", 2)
+linked_list_5.append("third", 3)
+print(linked_list_5.include("first") == True)
+
+print("returns false if a key is not in the list")
+print(linked_list_5.include("fourth") == False)
